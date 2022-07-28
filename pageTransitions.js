b0495c8d60998492e28810e6be821b192db6fdfe
monicaMDS <109643430+monicaMDS@users.noreply.github.com>
2022-07-28 23:13:06 +1200

    Base for Week 2
    Need to add images for week 2

--- pageTransitions.js
+++ pageTransitions.js
@@ -4,14 +4,24 @@ written by: Monica Luu
 **************************************************************/
 
 /**************************************************************
- BUTTON FUNCTION : changeMainPage()
- 1.switches from landing page to blog page
+ BUTTON FUNCTION : b1Switch()
+ 1.switches from posts page to week 1 blog post
 **************************************************************/
 function b1Switch() {
     console.log('b1Switch: switched page');
     document.getElementById("postsPage").style.display = "none";
     document.getElementById("blog_1").style.display = "block";
   };
+
+/**************************************************************
+ BUTTON FUNCTION : b2Switch()
+ 1.switches from posts page to week 1 blog post
+**************************************************************/
+function b2Switch() {
+  console.log('b2Switch: switched page');
+  document.getElementById("postsPage").style.display = "none";
+  document.getElementById("blog_2").style.display = "block";
+};
 
 /**************************************************************
  BUTTON FUNCTION : toTop()
